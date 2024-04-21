0016a962174a81056763757a5d84dfd790253d39
Cesar Pazzi <csrpazzi@gmail.com>
2024-04-21 02:17:27 -0600

    implemented windows notifications and button to go to PeakD

--- hive-notifications.ps1
+++ hive-notifications.ps1
@@ -8,36 +8,53 @@ limit = 100
 };
 id = 1
 } | ConvertTo-Json
-
+$account = (ConvertFrom-Json $jsonBody).params.account
 $response = Invoke-WebRequest -Uri $uri -Method Post -Body $jsonBody -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty result
 
-for ($i = 0; $i -lt $response.Count; $i++){
-    # reads $response.msg[$i] and checks if contains the words "replied to your post"
-    if ($response.msg[$i].Contains("replied to your post") -or $response.msg[$i].Contains("replied to your comment") -or $response.msg[$i].Contains("mentioned you"))
-    {
-        Write-Host $($response.id[$i])
-        Write-Host $($response.msg[$i])
-        Write-Host $($response.url[$i])
-        #from $response.url[$i] split text, simbol "/" is the braekpoint
-        $url = $response.url[$i].Split("/")
-        $username = $url[0].Split("@")
+$latest_local_id = Get-Content -Path "$account.txt" -ErrorAction SilentlyContinue | Select-Object -First 1
+$latest_id = $response[0].id
 
-        $get_reply = @{
-            jsonrpc = "2.0";
-            method = "condenser_api.get_content";
-            params = @($username[1],$url[1]);
-            id = 1
-            } | ConvertTo-Json
-        $reply_response = Invoke-WebRequest -Uri $uri -Method Post -Body $get_reply -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty result
+if ($latest_local_id -ne $latest_id){
+        for ($i = 0; $i -lt $response.Count; $i++){
+                if ($response.msg[$i].Contains("replied to your post") -or $response.msg[$i].Contains("replied to your comment") -or $response.msg[$i].Contains("mentioned you"))
+                { 
+                        if ($response.id[$i] -eq $latest_local_id){
+                                
+                                Write-Host "No new notifications for account $account"
+                                break
+                        }
+                        else{
+                                Write-Host "New notification found for account $account, id: $($response.id[$i])"
+                                Write-Host $($response.id[$i])
+                                Write-Host $($response.msg[$i])
+                                Write-Host $($response.url[$i])
 
-        # trim $reply_response.body to 65 characters
-        # IF $replay_response.body.length > 65, then it will be trimmed to 65 characters if not it will be printed as is
-        if ($reply_response.body.Length -gt 65){
-            Write-Host "$($reply_response.body.Substring(0,65))..."
+                                $url = $response.url[$i].Split("/")
+                                $username = $url[0].Split("@")
+
+                                $get_reply = @{
+                                        jsonrpc = "2.0";
+                                        method = "condenser_api.get_content";
+                                        params = @($username[1],$url[1]);
+                                        id = 1
+                                        } | ConvertTo-Json
+                                $reply_response = Invoke-WebRequest -Uri $uri -Method Post -Body $get_reply -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty result
+
+                                if ($reply_response.body.Length -gt 65){
+                                        $trimmed_response = "$($reply_response.body.Substring(0,65))..."
+                                        Write-Host $trimmed_response
+                                        New-BurntToastNotification -Header (New-BTHeader -Title "Hive Notifications") -Button (New-BTButton -Content "See in PeakD" -Arguments "https://peakd.com/$($response.url[$i])") -AppLogo ".\hive-logo.png" -Text "$($response.msg[$i])","$trimmed_response"
+                                }
+                                else{
+                                        Write-Host $reply_response.body
+                                        New-BurntToastNotification -Header (New-BTHeader -Title "Hive Notifications") -Button (New-BTButton -Content "See in PeakD" -Arguments "https://peakd.com/$($response.url[$i])") -AppLogo ".\hive-logo.png" -Text "$($response.msg[$i])","$($reply_response.body)"
+                                }
+                                Write-Host ""
+                        }
+                }
         }
-        else{
-            Write-Host $reply_response.body
-        }
-        Write-Host ""
-    }
 }
+else{
+        Write-Host "No new notifications for account $account"
+}
+$latest_id | Set-Content -Path "$account.txt" -Force
